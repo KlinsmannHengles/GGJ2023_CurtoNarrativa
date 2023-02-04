211715970d5a16290834af4608638236595e1bc8
KlinsmannHengles <klinsmannhengles@gmail.com>
2023-02-04 12:10:10 -0300

    Bug where the first line of dialogue dont appear

--- Assets/_Scripts/NPC/TestNPC.cs
+++ Assets/_Scripts/NPC/TestNPC.cs
@@ -19,9 +19,6 @@ public class TestNPC : GenericNPCBehaviour
         // Trigger dialogue
         DialogueManager.Instance.GetComponent<DialogueTrigger>().TriggerDialogue();
 
-        // Display sentence
-        DialogueManager.Instance.DisplayNextSentence();
-
     }
 
     public void FirstInteraction()
